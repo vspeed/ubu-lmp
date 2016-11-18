31ea3fb5cec2aa5fce8d41576b1b0bd98e3bd9d0
Viet Nguyen <vnguyen007@hotmail.com>
2016-11-18 15:20:01 -0800

    new info

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meata charset="UTF-8">
+<title> Getting Started With PHP</title>
+</head>
+<body>
+<?php
+# Write Traditional Greeting.
+echo '<h1>Hello World!</h1>' ;
+?>
+</body>
+</html>
